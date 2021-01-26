ea23622767eeac532c6bfcf08b09a44c25c52b53
h-ohshiro <h.ohshiro.1226@gmail.com>
2021-01-26 09:22:49 +0900

    スタッフ管理システムが完成した。

--- staff/staff_branch.php
+++ staff/staff_branch.php
@@ -1,5 +1,20 @@
 <?php
 
+if(isset($_POST['add']) == true){
+
+  header('Location:staff_add.php');
+  exit();
+}
+
+if (isset($_POST['disp']) == true) {
+  if (isset($_POST['staffcode']) == false) {
+    header('Location:staff_ng.php');
+    exit();
+  }
+  $staff_code=$_POST['staffcode'];
+  header('Location:staff_disp.php?staffcode='.$staff_code);
+
+}
 if(isset($_POST['edit']) == true){
 
   if(isset($_POST['staffcode'])==false){
@@ -10,6 +25,7 @@ if(isset($_POST['edit']) == true){
   header('Location:staff_edit.php?staffcode='.$staff_code);
   exit();
 }
+
 if(isset($_POST['delete']) == true){
 
   if(isset($_POST['staffcode'])==false){
@@ -18,7 +34,7 @@ if(isset($_POST['delete']) == true){
 
   $staff_code = $_POST['staffcode'];
   header('Location:staff_delete.php?staffcode='.$staff_code);
-  exit()；
+  exit();
 }
 
 ?>
