45ce0c81730d90d2eda3ee1b6683a9edf0ec1cd7
h-ohshiro <h.ohshiro.1226@gmail.com>
2021-01-28 18:04:27 +0900

    商品の参照、追加を作成した。

---
+++ product/pro_add.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ろくまる農園</title>
+</head>
+<body>
+
+  商品追加<br>
+  <br>
+  <form action="pro_add_check.php" method="post">
+  商品名を入力してください。<br>
+  <input type="text" name="name" style="width:200px"><br>
+  価格を入力してください。<br>
+  <input type="text" name="price" style="width: 100px;"><br>
+  <br>
+  <input type="button" onclick="history.back()" value="戻る">
+  <input type="submit" value="ＯＫ">
+  </form>
+
+</body>
+</html>
---
+++ product/pro_ng.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ろくまる農園</title>
+</head>
+<body>
+
+商品が選択されていません。<br>
+<a href="pro_list.php">戻る</a>
+
+</body>
+</html>
